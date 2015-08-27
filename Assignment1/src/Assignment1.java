e68bdf2209af0b43376ff20321c99ddb4b796178
jonathanmonreal <jem177@case.edu>
2015-08-27 12:27:51 -0400

    fix formatting, add binary

--- Assignment1/src/Assignment1.java
+++ Assignment1/src/Assignment1.java
@@ -1,10 +1,16 @@
+/**
+ * 
+ */
+
 import java.util.*;
 
-public class Assignment1
-{
-	public static <T> List<T> longestPrefix(List<T> a, List<T>b,
-											Comparator<? super T> cmp)
-	{
-		return null;
-	}
+/**
+ * @author jonmonreal
+ *
+ */
+public class Assignment1 {
+    public static <T> List<T> longestPrefix(List<T> a, List<T> b,
+	    Comparator<? super T> cmp) {
+	return null;
+    }
 }
