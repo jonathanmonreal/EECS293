f377ed7f528385016c1e51b599373fafc513b461
jonathanmonreal <jem177@case.edu>
2015-09-02 19:38:16 -0400

    add StringPrefix.java

--- Assignment1/src/LongestPrefix.java
+++ Assignment1/src/LongestPrefix.java
@@ -1,10 +1,15 @@
 import java.util.*;
 
 /**
+ * 
  * @author Jonathan Monreal
- *
  */
-public class LongestPrefix {
+public final class LongestPrefix {
+    
+    /**
+     * Do not allow this class to be instantiated.
+     */
+    private LongestPrefix() {}
     
     /**
      * 
@@ -15,7 +20,38 @@ public class LongestPrefix {
      */
     public static <T> List<T> longestPrefix(List<T> a, List<T> b,
 	    Comparator<? super T> cmp) {
-	return null;
+	if (eitherEmpty(a, b) || !areEqual(a.get(0), b.get(0), cmp))
+	    return new LinkedList<T>();
+	else {
+	    List<T> prefix = new LinkedList<T>();
+	    prefix.add(a.get(0));
+	    a.remove(0);
+	    b.remove(0);
+	    prefix.addAll(longestPrefix(a, b, cmp));
+	    return prefix;
+	}
+    }
+    
+    /**
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    private static <T> boolean eitherEmpty(List<T> a, List<T> b) {
+	return a.isEmpty() || b.isEmpty();
+    }
+    
+    /**
+     * 
+     * @param a
+     * @param b
+     * @param cmp
+     * @return
+     */
+    private static <T> boolean areEqual(T a, T b,
+	    Comparator<? super T> cmp) {
+	return cmp.compare(a, b) == 0;
     }
     
 }
---
+++ Assignment1/src/StringPrefix.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+/**
+ * 
+ * @author jonmonreal
+ */
+public class StringPrefix {
+    
+    private static String getStringPrefix(String a, String b) {
+	List<Character> prefix = LongestPrefix.longestPrefix(stringToList(a),
+		stringToList(b), null);
+	return null;
+    }
+    
+    private static List<Character> stringToList(String s) {
+	
+    }
+    
+}
