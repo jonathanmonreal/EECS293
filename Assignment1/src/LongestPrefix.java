5502c0101330ce0fe16f09e216ce53ef6fb7d675
jonathanmonreal <jem177@case.edu>
2015-09-02 16:47:44 -0400

    add comment structure

--- Assignment1/src/LongestPrefix.java
+++ Assignment1/src/LongestPrefix.java
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 import java.util.*;
 
 /**
@@ -9,8 +5,17 @@ import java.util.*;
  *
  */
 public class Assignment1 {
+    
+    /**
+     * 
+     * @param a
+     * @param b
+     * @param cmp
+     * @return
+     */
     public static <T> List<T> longestPrefix(List<T> a, List<T> b,
 	    Comparator<? super T> cmp) {
 	return null;
     }
+    
 }
